f8254e30f133078d6d34950c44cb655f9c5019ec
Pavan <t.hemant1992@gmail.com>
2020-10-20 13:23:57 +0530

    Added New activity into library and import lib to root app

---
+++ widgetkitlibrary/src/main/java/com/example/widgetkitlibrary/WidgetKitLibraryActivity.kt
@@ -0,0 +1,11 @@
+package com.example.widgetkitlibrary
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class WidgetKitLibraryActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_widget_kit_library)
+    }
+}
